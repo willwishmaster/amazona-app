7f33f3ba273d6c52f98ee3ffc71781ce1366cc25
Wilson Velasquez <wilcvelasquez@gmail.com>
2022-05-31 23:38:40 -0500

    Heroku app

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,8 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req,res)=>{res.sendFile(path.join(__dirname, '/frontend/build/index.html'))});
+app.get('*', (req, res) =>res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
+
 
 // app.get('/', (req, res) => {
 //     res.send('Server is ready');
